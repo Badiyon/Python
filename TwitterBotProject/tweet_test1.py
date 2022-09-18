4dfdaf455dcc977cc1c4493569ea1949527b3a05
Badiyon <102627801+Badiyon@users.noreply.github.com>
2022-09-18 18:53:30 -0400

    Update tweet_test1.py

--- TwitterBotProject/tweet_test1.py
+++ TwitterBotProject/tweet_test1.py
@@ -5,11 +5,11 @@ import time
 
 tweetStr = "This is a test"
 tweetStr1 = "This is another test"
-apikey='TWdtedFsmDjmvBK8I8CnEU0tJ'
-apikeysecret='7pB8W5kn4yIVv3ilqpiwnWxy5Y6lLRKwgPFcIITU7MpjTj2qWm'
+apikey='***'
+apikeysecret='***'
 
-accesstoken='1546188306475630600-8WnWsSqZVleqJv7ONcD6dFZCNemhjS'
-accesstokensecret='OBnwnpq1pRPIXBsrrqcG5Xgvcn4GWjLr2Pm39aJR4lnbt'
+accesstoken='***'
+accesstokensecret='***'
 
 api = Twython(apikey, apikeysecret, accesstoken, accesstokensecret)
 
