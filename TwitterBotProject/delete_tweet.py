67557ba530cb9e7ae3ad92a15a74823771be702f
Badiyon <102627801+Badiyon@users.noreply.github.com>
2022-09-18 18:52:39 -0400

    Update delete_tweet.py

--- TwitterBotProject/delete_tweet.py
+++ TwitterBotProject/delete_tweet.py
@@ -3,11 +3,11 @@ from twython import Twython
 
 tweet_id = 1547352919657390081
 
-apikey='TWdtedFsmDjmvBK8I8CnEU0tJ'
-apikeysecret='7pB8W5kn4yIVv3ilqpiwnWxy5Y6lLRKwgPFcIITU7MpjTj2qWm'
+apikey='***'
+apikeysecret='***'
 
-accesstoken='1546188306475630600-8WnWsSqZVleqJv7ONcD6dFZCNemhjS'
-accesstokensecret='OBnwnpq1pRPIXBsrrqcG5Xgvcn4GWjLr2Pm39aJR4lnbt'
+accesstoken='***'
+accesstokensecret='***'
 
 api = Twython(apikey, apikeysecret, accesstoken, accesstokensecret)
 
